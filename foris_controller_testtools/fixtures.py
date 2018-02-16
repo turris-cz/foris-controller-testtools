f93869a5d4c15d7de6416579be2fb63ccb95f7d1
Stepan Henek <stepan.henek@nic.cz>
2018-02-16 17:12:41 +0100

    add router_notifications into modules

--- foris_controller_testtools/fixtures.py
+++ foris_controller_testtools/fixtures.py
@@ -112,7 +112,7 @@ def controller_modules():
     """
     return [
         "about", "data_collect", "web", "dns", "maintain", "password", "updater", "lan", "time",
-        "wan",
+        "wan", "router_notifications"
     ]
 
 
