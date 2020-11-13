db3cd76c504b2d4b7bc65104076ae1c1aec00e43
Martin Matějek <martin.matejek@nic.cz>
2020-11-13 01:38:28 +0100

    svupdater: make packages mock more flexible
    Add option to mock installed packages with fallback to default packages
    list.
    If /var/lib/opkg/state.json is available, use that as packages source.
    
    Add turrishw file for Turris Shield.

--- foris_controller_testtools/svupdater/packages.py
+++ foris_controller_testtools/svupdater/packages.py
@@ -17,6 +17,10 @@
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 #
 
+import json
+from pathlib import Path
+from ..utils import TURRISHW_ROOT
+
 
 class Status:
     PACKAGES = ["turris-version", "foo-alternative"]
@@ -24,15 +28,32 @@ class Status:
         "foo-alternative": "foo"
     }
 
+    CUSTOM_FILE_PATH = Path(TURRISHW_ROOT) / "usr/lib/opkg/status.json"
+
     @classmethod
     def installed(cls, package):
         ret = []
 
-        if package in cls.PACKAGES:
+        packages, provides = cls._load_packages()
+
+        if package in packages:
             ret.append(package)
 
-        for name, provides in cls.PROVIDES.items():
+        for name, provides in provides.items():
             if package == provides:
                 ret.append(name)
 
         return tuple(ret)
+
+    @classmethod
+    def _load_packages(cls):
+        if cls.CUSTOM_FILE_PATH.is_file():
+            with open(cls.CUSTOM_FILE_PATH, 'r') as f:
+                data = json.load(f)
+
+            installed = data["installed"]
+            provides = {item["name"]: item["provides"] for item in data["provides"]}
+
+            return installed, provides
+
+        return cls.PACKAGES, cls.PROVIDES
