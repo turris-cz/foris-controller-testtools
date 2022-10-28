503f1ad17f126951b7916b1ecf3d1aba62585148
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-28 08:37:25 +0000

    Adding tarfile member sanitization to extractall()

--- foris_controller_testtools/utils.py
+++ foris_controller_testtools/utils.py
@@ -734,4 +734,23 @@ def prepare_turrishw(root: str):
         pass
     path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "turrishw", "%s.tar.gz" % root)
     with tarfile.open(path, "r:gz") as tar:
-        tar.extractall(TURRISHW_ROOT)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, TURRISHW_ROOT)
